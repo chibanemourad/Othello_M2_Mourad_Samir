9c28e51cbdb1d7767600fe8d0ab861918adbf985
Mourad CHIBANE <chimou_rabane@hotmail.fr>
2013-01-31 14:08:03 +0100

    Step-1
    Exportation du projet sur github, et début d'implémentation des tests

---
+++ src/OteloTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class OteloTest {
+
+	@Test
+	public final void testAjouterPion() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testOtelo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAfficher() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testReg_hor() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testReg_ver() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testReg_diag() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ src/Pion.java
@@ -0,0 +1,50 @@
+
+public class Pion {
+	private char Etat;
+	private int Px;
+	private int Py;
+	public Pion(char c) {
+		// TODO Auto-generated constructor stub
+		this.Etat=c;
+		this.Px=-1;
+		this.Py=-1;
+	}
+ public void placerPion(Pion p) {
+		// TODO Auto-generated constructor stub
+	 this.Etat=p.getEtat();
+	 this.Py=p.getPy();
+	 this.Px=p.getPx();
+	}
+	public Pion(char c,int x,int y)
+	{
+		this.Etat=c;
+		this.Px=x;
+		this.Py=y;
+		
+	}
+	public char getEtat() {
+		return Etat;
+	}
+	public void setEtat(char etat) {
+		Etat = etat;
+	}
+	public int getPx() {
+		return Px;
+	}
+	public void setPx(int px) {
+		Px = px;
+	}
+	public int getPy() {
+		return Py;
+	}
+	public void setPy(int py) {
+		Py = py;
+	}
+	
+ public void afficher_Pion()
+ {
+	 System.out.println("Etat "+ Etat);
+	 System.out.println("X "+Px);
+	 System.out.println("Y "+Py);
+ }
+}
