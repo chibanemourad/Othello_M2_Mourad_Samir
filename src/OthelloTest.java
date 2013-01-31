78fe5636018ff5550107212fd3ab2a067209031e
Mourad CHIBANE <chimou_rabane@hotmail.fr>
2013-01-31 16:37:07 +0100

    Test_Regle_TransformationHoriz
    Le Test "testReg_hor()" est réussit, ce test nous sert à vérifier si une
    transformation Horizontale est possible? il l'applique:il ne l'applique
    pas
    Remarque : dans ce test case nous avons tester les deux transformation
    possible c'est à dire : coté gauche ou coté droit.

--- src/OthelloTest.java
+++ src/OthelloTest.java
@@ -20,13 +20,11 @@ protected Othello oth_test;
 	@Test
 	public void testAjouterPion() {
 		Othello othe_1 = new Othello();
-		othe_1.afficher();
 		othe_1.getOt()[3][5].setEtat('B');
 		othe_1.getOt()[3][5].setPx(3);
 		othe_1.getOt()[3][5].setPy(5);
 		othe_1.getOt()[3][4].setEtat('B');
 		System.out.println();
-		oth_test.afficher();
 		oth_test.ajouterPion(new Pion('B',3,5));
 	
 		
@@ -36,7 +34,6 @@ protected Othello oth_test;
 				Assert.assertTrue("Le test s'est bien passé ",oth_test.getOt()[i][j].getEtat()==othe_1.getOt()[i][j].getEtat());
 				Assert.assertTrue("Le test s'est bien passé ",oth_test.getOt()[i][j].getPx()==othe_1.getOt()[i][j].getPx());
 				Assert.assertTrue("Le test s'est bien passé ",oth_test.getOt()[i][j].getPy()==othe_1.getOt()[i][j].getPy());
-				//assertEquals(oth_test.getOt()[3][5], othe_1.getOt()[3][5]);
 			}
 		}
 		
@@ -48,20 +45,46 @@ protected Othello oth_test;
 		fail("Not yet implemented");
 	}
 
-	/**
-	 * Test du MAJ du plateau.
-	 */
-	@Test
-	public void testAfficher() {
-//		fail("Not yet implemented");
-//		System.out.println("Test de mise a jour du plateau ");
-//		Othello instance = new Othello();
-//		instance.afficher();
-	}
-
 	@Test
 	public void testReg_hor() {
-		fail("Not yet implemented");
+		/*Test pour vérification de la regle de transformation horizontale coté gauche*/
+		Othello othe_1 = new Othello();
+		othe_1.getOt()[4][5]=new Pion('N', 4, 5);
+		othe_1.reg_hor(new Pion('N', 4, 5));
+		
+		oth_test.getOt()[4][5].setEtat('N');
+		oth_test.getOt()[4][5].setPx(4);
+		oth_test.getOt()[4][5].setPy(5);
+		oth_test.getOt()[4][4].setEtat('N');
+		for (int j=0; j<8;j++){
+			for(int i=0;i<8;i++ ){
+				Assert.assertTrue(oth_test.getOt()[i][j].getEtat()==othe_1.getOt()[i][j].getEtat());
+				Assert.assertTrue(oth_test.getOt()[i][j].getPx()==othe_1.getOt()[i][j].getPx());
+				Assert.assertTrue(oth_test.getOt()[i][j].getPy()==othe_1.getOt()[i][j].getPy());
+				
+			}
+			}
+		
+		/*Test pour vérification de la regle de transformation horizontale coté droit*/
+		
+		othe_1 = new Othello();
+		oth_test = new Othello();
+		othe_1.getOt()[3][2]=new Pion('N', 3, 2);
+		othe_1.reg_hor(new Pion('N', 3, 2));
+			
+		oth_test.getOt()[3][2].setEtat('N');
+		oth_test.getOt()[3][2].setPx(3);
+		oth_test.getOt()[3][2].setPy(2);
+		oth_test.getOt()[3][3].setEtat('N');
+		for (int j=0; j<8;j++){
+			for(int i=0;i<8;i++ ){
+				Assert.assertTrue(oth_test.getOt()[i][j].getEtat()==othe_1.getOt()[i][j].getEtat());
+				Assert.assertTrue(oth_test.getOt()[i][j].getPx()==othe_1.getOt()[i][j].getPx());
+				Assert.assertTrue(oth_test.getOt()[i][j].getPy()==othe_1.getOt()[i][j].getPy());
+
+			}
+			}
+		
 	}
 
 	@Test
