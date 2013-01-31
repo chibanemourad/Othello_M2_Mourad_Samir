8afe99b347fa604a819cb0f63571fb7ff37abb33
Mourad CHIBANE <chimou_rabane@hotmail.fr>
2013-01-31 14:25:45 +0100

    Step_2
    Implémentation des tests et réucriture de quelques méthodes

--- src/Executer.java
+++ src/Executer.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class Executer {
 
 	/**
@@ -8,52 +7,53 @@ public class Executer {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Otelo o=new Otelo();
-		int i=0,k=-1,l=-1;
-		Scanner sc=new Scanner(System.in);
-		boolean joueur=true;
-	  while(true)
-	  {  
-		  if(joueur)
-			  System.out.println("Tour de Noir    --->");
-		  else System.out.println("Tour de Blanc  --->");
-		  System.out.println("Poser un pion choisir :1");
-		  System.out.println("Terminer le jeu       :2");
-		  System.out.println("Abondonner Le jeu     :3");
-		  System.out.print("Votre choix           :");
-		  i=sc.nextInt();
-		  switch(i)
-		  {
-		  	case 1:		
-		  				while(k<0||k>7)
-					  	{
-					  		System.out.print("saisissez la valeur de x  <valeur  entre 0 et 7>:");
-					  		k=sc.nextInt();
-					  	}
-					  	while(l<0||l>7)
-					  	{
-					  		System.out.print("saisissez la valeur de y  <valeur  entre 0 et 7>:");
-					  		l=sc.nextInt();
-					  	}
-					  	if(joueur)
-					  		{
-					  		
-					  		  joueur=(!o.ajouterPion(new Pion('N',k,l)));
-					  		  System.out.println("la valeur de joueur est "+ joueur);
-					  		}
-					  	else {
-					  		  joueur=o.ajouterPion(new Pion('B',k,l));
-					  		}
-					  	k=-1;l=-1;
-					  	break;
-		  	case 2: System.out.println("le compte est ");
-		  			break;
-		  	case 3: if(joueur)
-		  		     System.out.println("Noir a gagné");
-		  			else System.out.println("Blanc a gagné");
-		  			System.exit(0);
-		  }
-	  }
+		Othello o = new Othello();
+		int i = 0, k = -1, l = -1;
+		Scanner sc = new Scanner(System.in);
+		boolean joueur = true;
+		while (true) {
+			if (joueur)
+				System.out.println("Tour de Noir    --->");
+			else
+				System.out.println("Tour de Blanc  --->");
+			System.out.println("Poser un pion choisir :1");
+			System.out.println("Terminer le jeu       :2");
+			System.out.println("Abondonner Le jeu     :3");
+			System.out.print("Votre choix           :");
+			i = sc.nextInt();
+			switch (i) {
+			case 1:
+				while (k < 0 || k > 7) {
+					System.out
+							.print("saisissez la valeur de x  <valeur  entre 0 et 7>:");
+					k = sc.nextInt();
+				}
+				while (l < 0 || l > 7) {
+					System.out
+							.print("saisissez la valeur de y  <valeur  entre 0 et 7>:");
+					l = sc.nextInt();
+				}
+				if (joueur) {
+
+					joueur = (!o.ajouterPion(new Pion('N', k, l)));
+					System.out.println("la valeur de joueur est " + joueur);
+				} else {
+					joueur = o.ajouterPion(new Pion('B', k, l));
+				}
+				k = -1;
+				l = -1;
+				break;
+			case 2:
+				System.out.println("le compte est ");
+				break;
+			case 3:
+				if (joueur)
+					System.out.println("Noir a gagné");
+				else
+					System.out.println("Blanc a gagné");
+				System.exit(0);
+			}
+		}
 	}
 
 }
---
+++ src/OthelloTest.java
@@ -0,0 +1,53 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class OthelloTest {
+
+	@Before
+	public static void setUp() throws Exception {
+	}
+
+	@After
+	public static  void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAjouterPion() {
+		
+	}
+
+	@Test
+	public void testOtelo() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test du MAJ du plateau.
+	 */
+	@Test
+	public void testAfficher() {
+		fail("Not yet implemented");
+		System.out.println("Test de mise à jour du plateau ");
+		Othello instance = new Othello();
+	}
+
+	@Test
+	public void testReg_hor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReg_ver() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReg_diag() {
+		fail("Not yet implemented");
+	}
+
+}
